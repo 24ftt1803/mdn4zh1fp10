5f570f80429d07be1c155cb027105005fcd65189
24ftt1803 <24ftt1803@student.pb.edu.bn>
2025-10-26 19:37:18 +0800

    Create GreatestCommonDivisor.java

---
+++ GreatestCommonDivisor.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class GreatestCommonDivisor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first integer: ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter second integer: ");
+        int num2 = input.nextInt();
+
+        int gcd = findGCD(num1, num2);
+
+        System.out.println("The greatest common divisor for " + num1 + " and " + num2 + " is " + gcd);
+        input.close();
+    }
+
+    public static int findGCD(int n1, int n2) {
+        int gcd = 1;
+        int k = 2;
+        while (k <= n1 && k <= n2) {
+            if (n1 % k == 0 && n2 % k == 0)
+                gcd = k;
+            k++;
+        }
+        return gcd;
+    }
+}
